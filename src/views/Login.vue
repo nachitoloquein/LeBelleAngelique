346a6732bf7c453a7e0d085a1ee5e981f7058b56
nachito <elnachoescachondo@gmail.com>
2021-11-13 17:43:44 -0300

    limpieza

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -4,38 +4,31 @@
 			<div class="wrap-login100">
 				<form class="login100-form validate-form">
 					<span class="login100-form-title p-b-34">
-						Account Login
+						Iniciar Sesión
 					</span>
 					
-					<div class="wrap-input100 rs1-wrap-input100 validate-input m-b-20" data-validate="Type user name">
-						<input id="first-name" class="input100" type="text" name="username" placeholder="User name">
+					<div class="wrap-input100 rs1-wrap-input100 validate-input m-b-20" data-validate="Ingrese su correo electrónico">
+						<input id="first-name" class="input100" type="email" name="username" placeholder="Correo electrónico">
 						<span class="focus-input100"></span>
 					</div>
 					<div class="wrap-input100 rs2-wrap-input100 validate-input m-b-20" data-validate="Type password">
-						<input class="input100" type="password" name="pass" placeholder="Password">
+						<input class="input100" type="password" name="pass" placeholder="Contraseña">
 						<span class="focus-input100"></span>
 					</div>
 					
 					<div class="container-login100-form-btn">
 						<button class="login100-form-btn">
-							Sign in
+							Entrar
 						</button>
 					</div>
 
 					<div class="w-full text-center p-t-27 p-b-239">
 						<span class="txt1">
-							Forgot
 						</span>
 
-						<a href="#" class="txt2">
-							User name / password?
-						</a>
 					</div>
 
 					<div class="w-full text-center">
-						<a href="#" class="txt3">
-							Sign Up
-						</a>
 					</div>
 				</form>
 				<div class="login100-more"></div>
